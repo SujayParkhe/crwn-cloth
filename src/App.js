0d10639045d745fe9104915206564b2a1b4381f2
lycanzzz <62604089+lycanzzz@users.noreply.github.com>
2021-02-17 19:11:37 +0530

    broken homepage into smaller components and finished styling our menu items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './components/HomePage/HomePage.jsx';
+import HomePage from './pages/HomePage/HomePage.jsx';
 
 function App() {
   return (
